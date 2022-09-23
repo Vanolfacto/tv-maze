b5608c18d8e9bb3e2a2bcb1d6bacee932bc57c65
Vanolfacto <vanjastojilkovic10@gmail.com>
2022-09-23 13:45:10 +0200

    main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { getData } from "../Helpers/helpers";
-import "./home.css";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getData } from '../Helpers/helpers';
+import './home.css';
 const Home = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    getData("/shows", setData);
+    getData('/shows', setData);
   }, []);
 
   return (
-    <ul className="list">
-      {data.slice(0, 50).map((show, index) => (
-        <li className="name" key={index}>
-          <Link to={`/show/${show.id}`} className="link">
-            <img className="image" src={show.image.medium} alt="" />
-            <p className="minititle">{show.name}</p>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul className="list">
+        {data.slice(0, 50).map((show, index) => (
+          <li className="name" key={index}>
+            <Link to={`/show/${show.id}`} className="link">
+              <img className="image" src={show.image.medium} alt="" />
+              <p className="minititle">{show.name}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div>Footer</div>
+    </>
   );
 };
 
